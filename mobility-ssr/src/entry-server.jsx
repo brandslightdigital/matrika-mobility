8fa8aa4aaf9e4e90dd96080678259d04fa386a80
R-Coder0 <rishabhpratap008@gmail.com>
2025-10-14 18:56:12 +0530

    focu

--- mobility-ssr/src/entry-server.jsx
+++ mobility-ssr/src/entry-server.jsx
@@ -1,16 +1,22 @@
-import { StrictMode } from 'react'
+// src/entry-server.jsx
+import React from 'react'
 import { renderToPipeableStream } from 'react-dom/server'
 import { StaticRouter } from 'react-router-dom/server'
 import App from './App'
 import './index.css'
 
 export function render(url, options) {
-  return renderToPipeableStream(
-    <StrictMode>
+  const app = (
+    <React.StrictMode>
       <StaticRouter location={url}>
         <App />
       </StaticRouter>
-    </StrictMode>,
-    options,
+    </React.StrictMode>
   )
+
+  return renderToPipeableStream(app, {
+    onShellReady: options.onShellReady,
+    onShellError: options.onShellError,
+    onError: options.onError,
+  })
 }
