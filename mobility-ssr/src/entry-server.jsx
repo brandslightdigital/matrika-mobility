6d8e0106b041ba3a81801c7ab8a044172f6a2170
R-Coder0 <rishabhpratap008@gmail.com>
2025-10-14 19:29:55 +0530

    changes

--- mobility-ssr/src/entry-server.jsx
+++ mobility-ssr/src/entry-server.jsx
@@ -1,22 +1,16 @@
-// src/entry-server.jsx
-import React from 'react'
+import { StrictMode } from 'react'
 import { renderToPipeableStream } from 'react-dom/server'
 import { StaticRouter } from 'react-router-dom/server'
 import App from './App'
 import './index.css'
 
 export function render(url, options) {
-  const app = (
-    <React.StrictMode>
+  return renderToPipeableStream(
+    <StrictMode>
       <StaticRouter location={url}>
         <App />
       </StaticRouter>
-    </React.StrictMode>
+    </StrictMode>,
+    options,
   )
-
-  return renderToPipeableStream(app, {
-    onShellReady: options.onShellReady,
-    onShellError: options.onShellError,
-    onError: options.onError,
-  })
 }
